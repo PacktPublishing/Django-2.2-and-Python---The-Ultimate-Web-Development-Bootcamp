3643e12fe0fb24fcac96035e64b1a034fea03f75
packtbipin2377 <44016603+packtbipin2377@users.noreply.github.com>
2019-08-08 15:37:37 +0530

    Code Deleted
    Code Deleted

--- test/portfolio-project/jobs/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-from .models import Job
-
-def home(request):
-    jobs  = Job.objects
-    return render(request,'jobs/home.html', {'jobs':jobs})
